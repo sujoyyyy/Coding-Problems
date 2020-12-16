e91fa0f2e2c7563504813d5ae4ef052a89e72259
Sujoy Datta <sujoydatta.2009@gmail.com>
2020-12-17 00:51:05 +0530

    changes

--- Array/maxmin.cpp
+++ Array/maxmin.cpp
@@ -1,2 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>ar[i];
+    }
+    int max=ar[0],min=ar[0];
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        if(max<ar[i]) max=ar[i];
+        if(min>ar[i]) min=ar[i];
+    }
+    cout<<"MAX : "<<max<<" MIN : "<<min;
+    return 0;
+}
+    
